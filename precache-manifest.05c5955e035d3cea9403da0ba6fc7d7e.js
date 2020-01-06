7df97bcb9bf3be09e4ed97831ed95fc62ec6acd2
SANJAY-YADAV18 <yadavs180396@gmail.com>
2020-01-06 15:32:41 +0530

    Updates

---
+++ precache-manifest.05c5955e035d3cea9403da0ba6fc7d7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26f01c19f67026c454e41f39bdb369e2",
+    "url": "/Quizbee/index.html"
+  },
+  {
+    "revision": "3c398c09a45adc0de889",
+    "url": "/Quizbee/static/css/main.444631b1.chunk.css"
+  },
+  {
+    "revision": "a71849bd7bb6eb95f65a",
+    "url": "/Quizbee/static/js/2.98813178.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Quizbee/static/js/2.98813178.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3c398c09a45adc0de889",
+    "url": "/Quizbee/static/js/main.b9104fa3.chunk.js"
+  },
+  {
+    "revision": "cc83e8495668aa763360",
+    "url": "/Quizbee/static/js/runtime-main.094230ca.js"
+  }
+]);
